46380e415ad9a95a8d1aafa8e0abf11e8dad5af1
Bohan <bohan.huang@coolbe.com.tw>
2019-10-03 18:11:42 +0800

    新增：modal

--- modal/js/index.js
+++ modal/js/index.js
@@ -1,15 +1,22 @@
-//gotop
-$(function(){
-  $('#gotop').on('click',function(){
-    $('html,body').animate({
-      scrollTop: 0
-    },400);
-  });
-  $(window).on('scroll',function(){
-    if($(this).scrollTop()>300){
-      $('#gotop').fadeIn('fast');
-    }else{
-      $('#gotop').stop().fadeOut('fast');
-    }
-  });
-})
+$(function() {
+	$('.modalOpen').on('click',function(){
+		openModal();
+	})
+	$('.modalClose').on('click',function(){
+		closeModal();
+	})
+	$(window).on('click',function(e){
+		if(e.target == $(".modal")[0]){
+			closeModal();
+		}
+	})
+});
+
+function openModal(){
+	$('.modal').fadeIn(300);
+	// $('body').css('overflow','hidden')
+}
+function closeModal(){
+	$('.modal').fadeOut(200);
+	// $('body').css('overflow','auto')
+}
